48f55770771442f16c475783a4b4aa51147a7890
Louis Pilfold <louis@lpil.uk>
2016-12-06 17:51:04 +0000

    ShimClient behaviour

--- functions/hello/lib/hello.ex
+++ functions/hello/lib/hello.ex
@@ -9,7 +9,7 @@ defmodule Hello do
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: Hello.Worker.start_link(arg1, arg2, arg3)
-      worker(Hello.StdioListener, []),
+      worker(Hello.Main, []),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
@@ -19,32 +19,65 @@ defmodule Hello do
   end
 end
 
-
-# Port = open_port({fd, 0, 1}, [in, binary, {line, 4096}]),
-
-defmodule Hello.StdioListener do
-  use GenServer
-
-  defstruct [:port]
+defmodule Hello.Main do
+  @behaviour AwsLambda.ShimClient
 
   def start_link do
-    GenServer.start_link(__MODULE__, [])
+    AwsLambda.ShimClient.start_link(__MODULE__, :this_is_mod_state)
+  end
+
+  def init(state) do
+    state
+  end
+
+  def handle_invocation(data, state) do
+    {:ok, %{data: data, state: state}, state}
+  end
+end
+
+defmodule AwsLambda.ShimClient do
+  use GenServer
+
+  defstruct [:port, :module, :module_state]
+
+  @type input :: any
+  @type reply :: any
+  @type state :: any
+  @callback init(any) :: {:ok, state}
+  @callback handle_invocation(input, state) :: {:ok, reply, state}
+                                             | {:error, reply, state}
+
+  def start_link(mod, mod_args) do
+    GenServer.start_link(__MODULE__, {mod, mod_args})
   end
 
   #
   # Callbacks
   #
 
-  def init(_) do
-    stdio = {:fd, 0, 1}
-    port = Port.open(stdio, [:binary, :in])
-    {:ok, %__MODULE__{port: port}}
+  def init({mod, mod_args}) do
+    port = Port.open({:fd, 0, 1}, [:binary])
+    mod_state = mod.init(mod_args)
+    state = %__MODULE__{port: port,
+                        module: mod,
+                        module_state: mod_state}
+    {:ok, state}
   end
 
   def handle_info({port, {:data, data}}, state = %{port: port}) do
-    IO.write """
-    {"value":{"hello":""}}
-    """
-    {:noreply, state}
+    input = decode_json(data)
+    # TODO error tuple handling
+    {:ok, resp, mod_state} =
+      state.module.handle_invocation(input, state.module_state)
+    output = Poison.encode!(resp) <> "\n"
+    Port.command(port, output)
+    {:noreply, %{state | module_state: mod_state}}
+  end
+
+  defp decode_json(json) do
+    case Poison.decode(json) do
+      {:ok, res} -> res
+      _ -> nil
+    end
   end
 end
--- functions/hello/mix.exs
+++ functions/hello/mix.exs
@@ -10,24 +10,15 @@ defmodule Hello.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
      mod: {Hello, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:distillery, "~> 0.0", only: [:dev]}]
+    [# Release build lib
+     {:distillery, "~> 0.0", only: [:dev]},
+     # JSON decoder
+     {:poison, "~> 3.0"}]
   end
 end
